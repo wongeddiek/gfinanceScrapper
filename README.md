099d42b3ca3e550ab91ad5721c76b8508a3881b3
Edward Wong <wong.eddiek@gmail.com>
2017-09-06 19:53:35 -0600

    added README

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a simple HTML scrapper getting the major financial market indexes (Dow Jones, S&P500, NASDAQ) data from www.google.com.
+
+It uses node request and cheerio libraries.
--- app.js
+++ app.js
@@ -10,23 +10,29 @@ function gfinance(url) {
     }
     //load the html DOM to cheerio
     const $ = cheerio.load(body);
-    var indexData = {
+    const IndexData = function(name, number, change, percent) {
+      this.name = name;
+      this.number = number;
+      this.change = change;
+      this.percent = percent;
     };
+    console.log($('title').text());
 
     //printing and setting Dow Jones data
-    console.log($('div.bld a').eq(0).text() + ": " + $('#ref_983582_l').text() + " " + $('#ref_983582_c').text() + " " + $('#ref_983582_cp').text());
+    var dowJones = new IndexData($('div.bld a').eq(0).text(), $('#ref_983582_l').text(), $('#ref_983582_c').text(), $('#ref_983582_cp').text());
 
-    indexData.dow = $('div.bld a').eq(0).text() + ": " + $('#ref_983582_l').text() + " " + $('#ref_983582_c').text() + " " + $('#ref_983582_cp').text();
+    console.log(dowJones.name + ": " + dowJones.number + dowJones.change + dowJones.percent);
 
     //printing and setting S&P data
-    console.log($('div.bld a').eq(1).text() + ": " + $('#ref_626307_l').text() + " " + $('#ref_626307_c').text() + " " + $('#ref_626307_cp').text());
 
-    indexData.sp = $('div.bld a').eq(1).text() + ": " + $('#ref_626307_l').text() + " " + $('#ref_626307_c').text() + " " + $('#ref_626307_cp').text();
+    var sp500 = new IndexData($('div.bld a').eq(1).text(), $('#ref_626307_l').text(), $('#ref_626307_c').text(), $('#ref_626307_cp').text());
+
+    console.log(sp500.name + ": " + sp500.number + sp500.change + sp500.percent);
 
     //printing and setting NASDAQ
-    console.log($('div.bld a').eq(2).text() + ": " + $('#ref_13756934_l').text() + " " + $('#ref_13756934_c').text() + " " + $('#ref_13756934_cp').text());
+    var nasdaq = new IndexData($('div.bld a').eq(2).text(), $('#ref_13756934_l').text(), $('#ref_13756934_c').text(), $('#ref_13756934_cp').text());
 
-    indexData.nasdaq = $('div.bld a').eq(2).text() + ": " + $('#ref_13756934_l').text() + " " + $('#ref_13756934_c').text() + " " + $('#ref_13756934_cp').text();
+    console.log(nasdaq.name + ": " + nasdaq.number + nasdaq.change + nasdaq.percent);
 
     //compile the <p> tag text in the html
     // $('#Explanation').parent().nextAll('p').each( function() {
